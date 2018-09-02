31f9bce2f7e3662e179c32161a231d9b283a66bf
danielpoehle <d.poehle@gmx.de>
2018-09-02 19:03:46 +0200

    fix connections and missing trains

--- KZugMatcher.R
+++ KZugMatcher.R
@@ -2,12 +2,28 @@ setwd("/home/daniel/Dokumente/importKSS/")
 
 completeList <- read.csv2(file = "./KSS2013_46/KSS2013_46-Gesamtdeutschland_v05.csv", stringsAsFactors = F)
 completeList <- completeList[completeList$RUN20131114,]
-which(completeList$FIRST == completeList$LAST)
+#which(completeList$FIRST == completeList$LAST)
+
+circles <- completeList[completeList$FIRST == completeList$LAST, ]
 # filter all trains which have moer than 1 station
 completeList <- completeList[completeList$FIRST != completeList$LAST, ]
 
+# filter only sgv
 sgv <- completeList$PROD_MAIN %in% c(seq(50,79,1), seq(-79,-76,1), seq(-74,-50,1), -36)
 completeList <- completeList[sgv,]
+
+# filter out studien
+l1 <- strsplit(completeList$ID, "-")
+stud <- logical(length(l1))
+for(i in 1:length(l1)){
+  if(as.numeric(l1[[i]][2]) >= 900){
+    stud[i] <- T
+  }
+}
+
+completeList <- completeList[!stud,]
+
+write.csv2(completeList, file = "./KSS2013_46/CompleteSGVList_14-11_v11.csv", row.names = F)
 
 trainNumbers <- unique(completeList$MAIN)
 
@@ -28,29 +44,51 @@ for(i in 1:length(trainNumbers)){
     ds_first[grepl("\\s.$", tempFrame$FIRST)] <- gsub('.{1}$', '', tempFrame$FIRST[grepl("\\s.$", tempFrame$FIRST)])   
     ds_last <- tempFrame$LAST
     ds_last[grepl("\\s.$", tempFrame$LAST)] <- gsub('.{1}$', '', tempFrame$LAST[grepl("\\s.$", tempFrame$LAST)]) 
-    first <- tempFrame[!(ds_first %in% ds_last) & ds_last %in% ds_first,]
+    first_id <- which(!(ds_first %in% ds_last) & ds_last %in% ds_first)
+    first <- tempFrame[first_id,]
     last <- tempFrame[!(ds_last %in% ds_first) & ds_first %in% ds_last,]
     if(length(first$ID) <1){
-        #only a loop
-        first <- tempFrame[which.max(ds_last %in% ds_first),]
+        #only a loop, compare departure time
+        times <- strsplit(tempFrame$DEP, "#")
+        t_list <- numeric(0)
+        for(j in 1:length(times)){
+          ti <- unlist(strsplit(times[[j]][1], ":"))
+          t_list[j] <- as.numeric(ti[1])*3600 + as.numeric(ti[2])*60 + as.numeric(unlist(strsplit(ti[3], "\\."))[1])
+        }
+        first_id <- which.min(t_list)
+        first <- tempFrame[first_id,]
     }
     if(any(length(tempFrame$ID) > 1 & !(ds_first %in% ds_last) & !(ds_last %in% ds_first))){
         s <- which(!(ds_first %in% ds_last) & !(ds_last %in% ds_first))
-        if(length(s)>1){
-            s <- s[s!=1]
+        len_s <- length(s)
+        if(length(first$ID)< 1){
+          first <- tempFrame[s[1],]
+        }
+        if(len_s>1){
+            s <- s[s!=first_id]
         }
         for(x in s){
             f2 <- tempFrame[x,]
-            if(first$ID[1] == f2$ID){
+            if(len_s > 1 && first$ID[1] == f2$ID){
                 stop(paste(i, "identical train IDs"))
                 }
             #print(paste(i, f2))
             c2 <- f2$ID
-            while(f2$LAST[1] %in% tempFrame$FIRST){
-                f2 <- tempFrame[tempFrame$FIRST == f2$LAST[1],]
-                if(grepl(f2$ID[1], c2)){break()}
-                c2 <- paste(c2, f2$ID[1], sep = "#")
-                }
+            f2_id <- x
+            while(ds_last[f2_id] %in% ds_first){
+              f2_id <- which(ds_first == ds_last[f2_id])
+              if(length(f2_id)>1){
+                # if two sucessors take the one with different destinaton to first
+                # if both sucessors have different destinations take the first one
+                f2_id <- f2_id[(ds_last[f2_id] != ds_first[f2_id][1])][1]
+              }
+              f2 <- tempFrame[f2_id,]
+              if(grepl(f2$ID[1], c2)){
+                break()
+              }
+              c2 <- paste(c2, f2$ID[1], sep = "#")
+            }
+            
             additionalTrainNumber[i] <- paste(additionalTrainNumber[i], c2, sep = "$")
             }
         }
@@ -69,11 +107,21 @@ for(i in 1:length(trainNumbers)){
                         #both different starts and goals --> two different trains
                         # take the second tain and use the first one normal, see below: chain <- first$ID[1]
                         c2 <- first$ID[2]
-                        f2 <- first[2,]
-                        while(f2$LAST[1] %in% tempFrame$FIRST){
-                            f2 <- tempFrame[tempFrame$FIRST == f2$LAST[1],]
-                            if(grepl(f2$ID[1], c2)){break()}
-                            c2 <- paste(c2, f2$ID[1], sep = "#")
+                        f2_id <- first_id[2]
+                        f2 <- tempFrame[f2_id,]
+                        
+                        while(ds_last[f2_id] %in% ds_first){
+                          f2_id <- which(ds_first == ds_last[f2_id])
+                          if(length(f2_id)>1){
+                            # if two sucessors take the one with different destinaton to first
+                            # if both sucessors have different destinations take the first one
+                            f2_id <- f2_id[(ds_last[f2_id] != ds_first[f2_id][1])][1]
+                          }
+                          f2 <- tempFrame[f2_id,]
+                          if(grepl(f2$ID[1], c2)){
+                            break()
+                          }
+                          c2 <- paste(c2, f2$ID[1], sep = "#")
                         }
                         doubleTrainNumber[i] <- c2
                     }else if(first$FIRST[1] == first$FIRST[2] & first$LAST[1] == first$LAST[2]){
@@ -100,13 +148,31 @@ for(i in 1:length(trainNumbers)){
                 }
             }
         }
-    chain <- first$ID[1] # take only the first train
+    # take only the first train
+    first_id <- first_id[1]
+    chain <- first$ID[1]
     
-    while(first$LAST[1] %in% tempFrame$FIRST){
-        first <- tempFrame[tempFrame$FIRST == first$LAST[1],]
+    
+    while(ds_last[first_id] %in% ds_first){
+        first_id <- which(ds_first == ds_last[first_id])
+        if(length(first_id)>1){
+          # if two sucessors take the one with different destinaton to first
+          first_id <- first_id[(ds_last[first_id] != ds_first[first_id][1])]
+          if(length(first_id) > 1){
+            # if both sucessors have different destinations remove a dead end 
+            # if no dead end is there take the first sucessor
+            if(length(first_id[ds_last[first_id] %in% ds_first])>0){
+              first_id <- first_id[ds_last[first_id] %in% ds_first][1]
+            }else{
+              first_id <- first_id[1]
+            }
+          }
+        }
+        first <- tempFrame[first_id,]
         if(grepl(first$ID[1], chain)){
             loop[i] <- T
-            break()}
+            break()
+            }
         chain <- paste(chain, first$ID[1], sep = "#")
     }
     idList[i] <- chain
@@ -114,39 +180,63 @@ for(i in 1:length(trainNumbers)){
 
 addTrains <- unlist(strsplit(additionalTrainNumber[which(additionalTrainNumber!="FALSE")], "\\$"))
 addTrains <- addTrains[which(addTrains != "FALSE")]
-######################## check additional train numbers if they fit to master trains
-
-for(i in 1:length(addTrains)){
-    print(i)
-    aTrain <- addTrains[i]
-    main <- unlist(strsplit(aTrain, "-"))[1]
-    long <- df$connectingIDs[df$TrainNumber == main]
-    first <- completeList[completeList$ID ==unlist(strsplit(long, "#"))[1],]
-    last <- completeList[completeList$ID ==unlist(strsplit(long, "#"))[length(unlist(strsplit(long, "#")))],]
-    train <- completeList[completeList$ID ==aTrain,]
-    if(gsub('.{1}$', '', first$FIRST) == gsub('.{1}$', '', train$LAST) & nchar(train$LAST) == 5){
-        df$connectingIDs[df$TrainNumber == main] <- paste(aTrain, df$connectingIDs[df$TrainNumber == main], sep = "#")
-        addTrains[i] <- "FALSE"
-        print(paste("train", train$LAST, "first", first$FIRST))
-    }
-    
-    if(gsub('.{1}$', '', last$LAST) == gsub('.{1}$', '', train$FIRST) & nchar(train$LAST) == 5){
-        df$connectingIDs[df$TrainNumber == main] <- paste(df$connectingIDs[df$TrainNumber == main], aTrain, sep = "#")
-        addTrains[i] <- "FALSE"
-        print(paste("last", last$LAST, "train", train$FIRST))
-    }
-}
-
-addTrains <- addTrains[which(addTrains != "FALSE")]
 doubleTrains <-  doubleTrainNumber[doubleTrainNumber != "FALSE"]
 
+##### check if all train numbers are still 
+allIDs <- completeList$ID
+
+tmp <- (unlist(strsplit(idList, "#")))
+tmp <- c(addTrains, (unlist(strsplit(doubleTrains, "#"))), tmp)
+
+missing <- integer(0)
+m_id <- integer(0)
+for(i in 1:length(allIDs)){
+  if(!(allIDs[i] %in% tmp)){
+    missing <- c(missing, allIDs[i])
+    m_id <- c(m_id, i)
+  }
+}
+
+missing[7]
+idList[which(trainNumbers == 95283)]
+additionalTrainNumber[which(trainNumbers == 95283)]
+completeList[completeList$MAIN == "95283",1:21]
+
+write.csv2(missing, file = "./KSS2013_46/Missing_v11.csv", row.names = F)
+
+######################## check additional train numbers if they fit to master trains
+
+# for(i in 1:length(addTrains)){
+#     print(i)
+#     aTrain <- addTrains[i]
+#     main <- unlist(strsplit(aTrain, "-"))[1]
+#     long <- df$connectingIDs[df$TrainNumber == main]
+#     first <- completeList[completeList$ID ==unlist(strsplit(long, "#"))[1],]
+#     last <- completeList[completeList$ID ==unlist(strsplit(long, "#"))[length(unlist(strsplit(long, "#")))],]
+#     train <- completeList[completeList$ID ==aTrain,]
+#     if(gsub('.{1}$', '', first$FIRST) == gsub('.{1}$', '', train$LAST) & nchar(train$LAST) == 5){
+#         df$connectingIDs[df$TrainNumber == main] <- paste(aTrain, df$connectingIDs[df$TrainNumber == main], sep = "#")
+#         addTrains[i] <- "FALSE"
+#         print(paste("train", train$LAST, "first", first$FIRST))
+#     }
+#     
+#     if(gsub('.{1}$', '', last$LAST) == gsub('.{1}$', '', train$FIRST) & nchar(train$LAST) == 5){
+#         df$connectingIDs[df$TrainNumber == main] <- paste(df$connectingIDs[df$TrainNumber == main], aTrain, sep = "#")
+#         addTrains[i] <- "FALSE"
+#         print(paste("last", last$LAST, "train", train$FIRST))
+#     }
+# }
+# 
+# addTrains <- addTrains[which(addTrains != "FALSE")]
+
+
 df <- data.frame(TrainNumber = c(seq(1:length(addTrains)), 1000+seq(length(doubleTrains)), trainNumbers), connectingIDs = c(addTrains, doubleTrains, idList), stringsAsFactors = F)
-write.csv2(df, file = "./KSS2013_46/Connections_v10.csv", row.names = F)
+write.csv2(df, file = "./KSS2013_46/Connections_v11.csv", row.names = F)
 
 write.csv2(which(doubleTrainNumber == "TRUE"), file = "./KSS2013_46/DoubleTrainNumbers_v10.csv", row.names = F)
 write.csv2(which(loop), file = "./KSS2013_46/Loops_v10.csv", row.names = F)
 
-
+write.csv2(circles, file = "./KSS2013_46/Single_v11.csv", row.names = F)
 
 
 
